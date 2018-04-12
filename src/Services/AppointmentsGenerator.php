f6bf52bc057dacbb12b9f119cb5f63ce3030e2bc
Zathey92 <eliolistillo@gmail.com>
2018-04-12 15:25:34 +0100

    DayOfWeekGeneration. Mondays Hardcode

--- src/Services/AppointmentsGenerator.php
+++ src/Services/AppointmentsGenerator.php
@@ -31,6 +31,8 @@ class AppointmentsGenerator
     }
     public function generate($previousDate, $option)
     {
+        $day = 'Monday';
+
         if($option == "AM"){
             $initHour=9;
             $endHour=13;
@@ -42,26 +44,44 @@ class AppointmentsGenerator
         if(isset($previousDate)){
             $date = new \DateTime($previousDate);
 
+        }else if(isset($day)){
+            $date = new \DateTime($day);
         }else{
             $date = new \DateTime();
-    }
-        while(true){
-            if($date->format('H')<$initHour){
-                $date->setTime($initHour,0,0);
-            }
-                if($date->format('H')>$endHour){
-                    $date->modify("+1 day");
-                    $date->setTime($initHour,0,0);
-                }else{
-                    $date->modify("+1 hour");
-                }
-            if(empty($this->ar->findByDate($date))){
-                return $date;
-            }
-
         }
-        return null;
+        while(true){
+            if(isset($day)&&$date->format('l')!=$day){          //Si Dia de la semana es diferente y la opción está seleccionada
+                $interval = \DateInterval::createFromDateString($day); //Calculamos el intervalo relativalo al siguiente lunes
+                $date->add($interval);                                  //Se aplica el intervalo relativo a la fecha actual
+                $date->setTime($initHour, 0, 0);        //Reiniciamos la hora.
+            }
+            $bounds = $this->checkBounds($date,$initHour,$endHour);     //Calculamos extremos [-1,0,1]
+            if($bounds<0) {                                             //Si estamos por debajo del horario
+                $date->setTime($initHour, 0, 0);        //Reiniciamos la hora
+            }else if($bounds>0){                                        //Si estamos por encima
+                if(isset($day)) {                                       //Y la opción de día está activa
+                    $date->modify("+7 day");                    //Avanzamos 7 días
+                }else{
+                    $date->modify("+1 day");                    //Si no avanzamos 1 día
+                }
+                $date->setTime($initHour, 0, 0);        //Reiniciamos hora
+            }else{                                                     //Si estamos dentro de las opciones horarias/límites
+                $date->modify("+1 hour");                       //Avanzamos 1 hora
+            }
+            if(empty($this->ar->findByDate($date))){                    //Si la fecha no está ocupada
+                return $date;                                           //Salimos y Devolvemos la fecha.
+            }
+        }
+        return null;                                                    //Nunca debería ejecutarse
     }
+
+    private function checkBounds(\DateTime $date,$inf,$sup){
+        $date = $date->format('H');
+        if($date>=$sup) return 1;
+        if($date<$inf) return -1;
+        return 0;
+    }
+
     public function persist($user,$date){
         $doctor  = $this->dr->findByDni('doctor');
         $date = new \DateTime($date) ;
